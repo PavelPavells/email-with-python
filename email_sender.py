1fbb3c2f5cb4ec03efab397213ebc3e43968632d
Павел Смирнов <Pasha@MacBook-Air-Pavel.local>
2020-01-11 15:34:14 +0300

    [11.01.2020 - Work with Send Email with Python]

---
+++ email_sender.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path #os.path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Pavel Smirnov'
+email['to'] = 'andrej.a@ro.ru'
+email['subject'] = 'You won 1.000.000 dollars!'
+
+email.set_content(html.substitute({'name': 'Pavel'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('hismailforyou@gmail.com', 'Crysis123456')
+    smtp.send_message(email)
+    print('All good!')
