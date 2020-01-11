05e7373bf886b1ebea9c9950a49044d91e5343ba
SMIRNOV PAVEL <32220360+PavelPavells@users.noreply.github.com>
2020-01-11 23:01:04 +0300

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -14,6 +14,6 @@ email.set_content(html.substitute({'name': 'Pavel'}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('hismailforyou@gmail.com', 'Crysis123456')
+    smtp.login('hismailforyou@gmail.com', 'your_password')
     smtp.send_message(email)
     print('All good!')
